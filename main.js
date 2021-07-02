c9e33ad449a8c41189b4ccb2699b942cf2129de5
mdranacse19 <mdrana.rakibuddin@gmail.com>
2021-07-02 19:01:01 +0600

    How Request Response Model Works in node js

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const http = require('http');
 
 var serverCreate = http.createServer(function (request, response) {
-    response.end('I love programming');
+    if (request.url == '/'){
+        response.writeHead(200, {'Content-type':'text/html'});
+        response.write("<h2>This is home page</h2>");
+        response.end();
+
+    } else if (request.url == '/about'){
+        response.writeHead(200, {'Content-type':'text/html'});
+        response.write("<h2>This is about page</h2>");
+        response.end();
+
+    } else if (request.url == '/contact'){
+        response.writeHead(200, {'Content-type':'text/html'});
+        response.write("<h2>This is contact page</h2>");
+        response.end();
+
+    }
 })
 serverCreate.listen(1111);
 console.log('server created successfully');
