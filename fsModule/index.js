176ae53232a5c244901a5c30644460e766a15a7c
mdranacse19 <mdrana.rakibuddin@gmail.com>
2021-07-09 11:55:07 +0600

    async file wirte

--- fsModule/index.js
+++ fsModule/index.js
@@ -3,8 +3,7 @@ const HTTP = require('http');
 
 const createServer = HTTP.createServer(function (request, response) {
     if(request.url == "/"){
-        //file read
-        //async
+        //async file read
         /*FS.readFile('Home.html', function (error, data) {
             response.writeHead(200, {'Content-Type': 'text/html'})
             response.write(data);
@@ -16,8 +15,8 @@ const createServer = HTTP.createServer(function (request, response) {
         response.write(myData);
         response.end();*/
 
-        //file write
-        FS.writeFile('test.txt', 'welcome to node js platform', function (error) {
+        //async file write
+        FS.writeFile('test.txt', 'welcome to node js platform 2', function (error) {
             if (error) {
                 response.writeHead(404, {'Content-Type': 'text/html'})
                 response.write("File write fail")
