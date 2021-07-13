ef2ed3bf306af8ef8fbed098e5be853810018521
mdranacse19 <mdrana.rakibuddin@gmail.com>
2021-07-13 15:14:56 +0600

    create database

--- node_with_mysql/index.js
+++ node_with_mysql/index.js
@@ -12,6 +12,10 @@ connectDB.connect(function (error) {
     if (error){
         console.log('connection failed! '+error)
     }else{
+        connectDB.query("create database node_with_mysql", function (error, result) {
+            if(error) console.log('database created failed! '+error)
+            console.log('database created success!')
+        })
         console.log('connection success!')
     }
 
