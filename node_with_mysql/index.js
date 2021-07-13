320db783a9e3d9878f76a3b18ad87a7b36954ed9
mdranacse19 <mdrana.rakibuddin@gmail.com>
2021-07-13 15:40:19 +0600

    insert data into database

--- node_with_mysql/index.js
+++ node_with_mysql/index.js
@@ -1,10 +1,6 @@
 const mysql = require('mysql');
 
-const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-}
+const dbConfig = { host: 'localhost',user: 'root',password: '',database: 'node_with_mysql' }
 
 const connectDB = mysql.createConnection(dbConfig);
 
@@ -12,11 +8,17 @@ connectDB.connect(function (error) {
     if (error){
         console.log('connection failed! '+error)
     }else{
-        connectDB.query("create database node_with_mysql", function (error, result) {
-            if(error) console.log('database created failed! '+error)
-            console.log('database created success!')
-        })
         console.log('connection success!')
+        insertData(connectDB)
     }
 
 })
+
+//for insert data
+function insertData(conn) {
+    let sqlQuery = "INSERT INTO student_list(name, role, address, class, date) VALUES ('rana',10,'mirpur 1, dhaka',12,'2021-07-13')";
+    conn.query(sqlQuery, function (error) {
+        if(error) console.log("Query execute failed! " +error)
+        console.log("Query execute success! ")
+    })
+}
