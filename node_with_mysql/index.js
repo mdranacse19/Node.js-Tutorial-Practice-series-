c24478697f806a033b1d1b5d2e19bb1bbc0f98d7
mdranacse19 <mdrana.rakibuddin@gmail.com>
2021-07-19 14:08:17 +0600

    work with delete function

--- node_with_mysql/index.js
+++ node_with_mysql/index.js
@@ -9,14 +9,24 @@ connectDB.connect(function (error) {
         console.log('connection failed! '+error)
     }else{
         console.log('connection success!')
-        insertData(connectDB)
+        //insertData(connectDB)
+        deleteData(connectDB)
     }
 
 })
 
 //for insert data
 function insertData(conn) {
-    let sqlQuery = "INSERT INTO student_list(name, role, address, class, date) VALUES ('rana',10,'mirpur 1, dhaka',12,'2021-07-13')";
+    let sqlQuery = "INSERT INTO student_list(name, role, address, class, date) VALUES ('rana',13,'mirpur 1, dhaka',12,'2021-07-13')";
+    conn.query(sqlQuery, function (error) {
+        if(error) console.log("Query execute failed! " +error)
+        console.log("Query execute success! ")
+    })
+}
+
+//for delete data
+function deleteData(conn) {
+    let sqlQuery = "DELETE FROM student_list WHERE id = 5";
     conn.query(sqlQuery, function (error) {
         if(error) console.log("Query execute failed! " +error)
         console.log("Query execute success! ")
