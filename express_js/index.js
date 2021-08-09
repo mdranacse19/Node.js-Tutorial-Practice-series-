9be39ce051edb166b5fcf1ec76330da86c780697
mdranacse19 <mdrana.rakibuddin@gmail.com>
2021-08-09 16:45:25 +0600

    get and post request test

--- express_js/index.js
+++ express_js/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
-app = express();
 const fs = require('fs');
-
 const HTTP = require('http');
+const bodyParser = require('body-parser')
+const multer  = require('multer')
+const multerObj = multer()
+
+
+const app = express();
+//for body json
+app.use(bodyParser.json())
+//for form data - use for only form data
+//app.use(multerObj.array())
+//app.use(express.static('public'))
 
 //text response
 app.get('/one', function (request, response) {
@@ -59,6 +68,65 @@ app.get('/cookie_response_clear', function (request, response) {
     response.send('successfully run cookie clear response')
     response.status('200').end()
 })
+
+
+//FOR REQUEST QUERY
+app.get('/request_query', function (request, response) {
+
+    let firstName = request.query.firstName;
+    let lastName = request.query.lastName;
+    response.status(200).end('This is request_query. And the name is '+firstName+' '+lastName)
+})
+//request_header
+app.get('/request_header', function (request, response) {
+
+    let firstName = request.header('firstName');
+    let lastName = request.header('lastName');
+    let userAgent = request.header('User-Agent');
+    response.status(200).end('This is request_header(get request). And the name is '+firstName+' '+lastName+' And UserAgent is '+userAgent)
+})
+//post_request
+app.post('/post_request', function (request, response) {
+
+    let firstName = request.query.firstName;
+    let lastName = request.query.lastName;
+    response.status(200).end('This is request_query. And the name is '+firstName+' '+lastName)
+})
+//json_request
+app.post('/json_request', function (request, response) {
+    let jsonData = request.body;
+    let jsonString = JSON.stringify(jsonData);
+    let post = jsonData.post
+    let village = jsonData['village']
+    response.status(200).send(jsonString+' ---------- '+post+' ---------- '+village)
+})
+//form_data_request
+app.post('/form_data_request', function (request, response) {
+    let jsonData = request.body;
+    let jsonString = JSON.stringify(jsonData);
+    response.status(200).send(jsonString)
+})
+//file_upload_request
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './my-uploads')
+    },
+    filename:function (req, file, callback) {
+        callback(null, new Date().getSeconds()+'-'+file.originalname)
+    }
+})
+var upload = multer({storage:storage}).single('myFile');
+
+app.post('/file_upload_request', function (request, response) {
+    upload(request, response, function (error) {
+        if (error){
+            response.status(400).send('File upload fail!!' + error)
+        } else {
+            response.status(200).send('File upload success!!')
+        }
+    })
+})
+
 
 
 
